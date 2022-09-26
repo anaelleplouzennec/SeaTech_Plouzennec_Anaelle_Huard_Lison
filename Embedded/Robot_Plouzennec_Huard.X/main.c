13be7e930f3a9e125084c1db2d29878f58bbe97d
TABLE2 <78029790+Kergourlay-T@users.noreply.github.com>
2022-09-26 17:41:05 +0200

    séance1

---
+++ Embedded/Robot_Plouzennec_Huard.X/main.c
@@ -0,0 +1,48 @@
+/* 
+ * File:   main.c
+ * Author: GEII Robot
+ *
+ * Created on 26 septembre 2022, 14:38
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include "ChipConfig.h"
+#include "IO.h"
+#include "timer.h"
+#include "PWM.h"
+
+int main(void) {
+    /***************************************************************************************************/
+    //Initialisation de l?oscillateur
+    /****************************************************************************************************/
+    InitOscillator();
+
+    /****************************************************************************************************/
+    // Configuration des entrées sorties
+    /****************************************************************************************************/
+    InitIO();
+
+    LED_BLANCHE = 1;
+    LED_BLEUE = 1;
+    LED_ORANGE = 1;
+    
+     InitTimer23 () ;
+     InitTimer1 () ;
+     
+     InitPWM();
+     PWMSetSpeed(30);
+     
+    /****************************************************************************************************/
+    // Boucle Principale
+    /****************************************************************************************************/
+    while (1) {
+        //LED_BLANCHE = !LED_BLANCHE;
+        //LED_BLEUE = !LED_BLEUE;
+        //LED_ORANGE = !LED_ORANGE;
+    } // fin main
+    
+    
+}
+

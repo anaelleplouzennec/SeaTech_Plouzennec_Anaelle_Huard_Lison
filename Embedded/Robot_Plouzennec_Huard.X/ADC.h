d52b68655e2bf81939eac46216c53aeccf2fd4fc
TABLE2 <78029790+Kergourlay-T@users.noreply.github.com>
2022-09-28 12:11:12 +0200

    début analogique 2

--- Embedded/Robot_Plouzennec_Huard.X/ADC.h
+++ Embedded/Robot_Plouzennec_Huard.X/ADC.h
@@ -8,16 +8,12 @@
 #ifndef ADC_H
 #define	ADC_H
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
-
-
-
-#ifdef	__cplusplus
-}
-#endif
+void InitADC1(void);
+void __attribute__((interrupt, no_auto_psv)) _AD1Interrupt(void);
+void ADC1StartConversionSequence();
+unsigned int * ADCGetResult(void);
+unsigned char ADCIsConversionFinished(void);
+void ADCClearConversionFinishedFlag(void);
 
 #endif	/* ADC_H */
 
